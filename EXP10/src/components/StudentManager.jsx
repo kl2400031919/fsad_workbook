1c142c13d0a0a19575ce6083dff8e19c94025e70
kl2400031919 <2400031919@kluniversity.in>
2026-03-14 21:59:25 +0530

    commit
    commit

---
+++ EXP10/src/components/StudentManager.jsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import "./StudentManager.css";
+
+function StudentManager() {
+
+  const [students, setStudents] = useState([
+    { id: 1, name: "Deepika", course: "CSE" },
+    { id: 2, name: "Rahul", course: "Mechanical" },
+    { id: 3, name: "Anita", course: "ECE" },
+    { id: 4, name: "Kiran", course: "Civil" },
+    { id: 5, name: "Sneha", course: "IT" }
+  ]);
+
+  const [newStudent, setNewStudent] = useState({
+    id: "",
+    name: "",
+    course: ""
+  });
+
+  const handleChange = (e) => {
+    setNewStudent({
+      ...newStudent,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const addStudent = () => {
+
+    if (!newStudent.id || !newStudent.name || !newStudent.course) {
+      alert("Fill all fields");
+      return;
+    }
+
+    setStudents([...students, newStudent]);
+
+    setNewStudent({
+      id: "",
+      name: "",
+      course: ""
+    });
+  };
+
+  const deleteStudent = (id) => {
+    setStudents(students.filter((s) => s.id !== id));
+  };
+
+  return (
+    <div className="container">
+
+      <h2>Student Manager</h2>
+
+      <div className="form">
+
+        <input
+          type="number"
+          name="id"
+          placeholder="ID"
+          value={newStudent.id}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={newStudent.name}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          name="course"
+          placeholder="Course"
+          value={newStudent.course}
+          onChange={handleChange}
+        />
+
+        <button onClick={addStudent}>Add Student</button>
+
+      </div>
+
+      {students.length === 0 ? (
+        <p>No students available</p>
+      ) : (
+        <table>
+
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Course</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {students.map((s) => (
+              <tr key={s.id}>
+                <td>{s.id}</td>
+                <td>{s.name}</td>
+                <td>{s.course}</td>
+                <td>
+                  <button
+                    className="deleteBtn"
+                    onClick={() => deleteStudent(s.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+
+        </table>
+      )}
+
+    </div>
+  );
+}
+
+export default StudentManager;
